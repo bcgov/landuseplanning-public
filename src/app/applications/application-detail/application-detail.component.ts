60735dbe067b30a891059d2bf9300b7694567c27
Mark Lisé <mark@digitalspace.ca>
2018-03-12 14:29:22 -0700

    NOBUG: Adding in shape files, descriptions, adjustments to public view of application.

--- src/app/applications/application-detail/application-detail.component.ts
+++ src/app/applications/application-detail/application-detail.component.ts
@@ -8,6 +8,9 @@ import { Application } from '../../models/application';
 import { ApplicationService } from 'app/services/application.service';
 import { CommentPeriodService } from 'app/services/commentperiod.service';
 import { AddCommentComponent } from './add-comment/add-comment.component';
+import { SearchService } from '../../services/search.service';
+import * as L from 'leaflet';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-application-detail',
@@ -20,6 +23,9 @@ export class ApplicationDetailComponent implements OnInit, OnDestroy {
     { label: 'Comments', link: 'comments' },
     { label: 'Decisions', link: 'decisions' }
   ];
+  public layers: L.Layer[];
+  public fg: L.FeatureGroup;
+  public map: L.Map;
 
   public application: Application = null;
   private ngUnsubscribe: Subject<boolean> = new Subject<boolean>();
@@ -28,6 +34,7 @@ export class ApplicationDetailComponent implements OnInit, OnDestroy {
     private route: ActivatedRoute,
     private router: Router,
     private dialogService: DialogService,
+    private searchService: SearchService,
     private applicationService: ApplicationService, // used in template
     private commentPeriodService: CommentPeriodService // used in template
   ) { }
@@ -40,6 +47,64 @@ export class ApplicationDetailComponent implements OnInit, OnDestroy {
         (data: { application: Application }) => {
           if (data.application) {
             this.application = data.application;
+            const self = this;
+            this.searchService.getByDTID(this.application.tantalisID.toString()).subscribe(
+              features => {
+                self.map = L.map('map').setView([53.505, -127.09], 6);
+                console.log('map');
+                const World_Topo_Map = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+                  attribution: 'Tiles &copy; Esri &mdash; and the GIS User Community'
+                }).addTo(self.map);
+
+                if (self.fg) {
+                  _.each(self.layers, function (layer) {
+                    self.map.removeLayer(layer);
+                  });
+                  self.fg.clearLayers();
+                } else {
+                  self.fg = L.featureGroup();
+                }
+
+                _.each(features, function (feature) {
+                  const f = JSON.parse(JSON.stringify(feature));
+                  // Needed to be valid GeoJSON
+                  delete f.geometry_name;
+                  const featureObj: GeoJSON.Feature<any> = f;
+                  const layer = L.geoJSON(featureObj);
+                  const options = { maxWidth: 400 };
+                  // const content = '<h3>' + featureObj.properties.TENURE_TYPE
+                  //   + '<br />'
+                  //   + featureObj.properties.TENURE_SUBTYPE + '</h3>'
+                  //   + '<strong>ShapeID: </strong>' + featureObj.properties.INTRID_SID
+                  //   + '<br />'
+                  //   + '<strong>Disposition: </strong>' + featureObj.properties.DISPOSITION_TRANSACTION_SID
+                  //   + '<br />'
+                  //   + '<strong>Purpose: </strong>' + featureObj.properties.TENURE_PURPOSE
+                  //   + '<br />'
+                  //   + '<strong>Sub Purpose: </strong>' + featureObj.properties.TENURE_SUBPURPOSE
+                  //   + '<br />'
+                  //   + '<strong>Stage: </strong>' + featureObj.properties.TENURE_STAGE
+                  //   + '<br />'
+                  //   + '<strong>Status: </strong>' + featureObj.properties.TENURE_STATUS
+                  //   + '<br />'
+                  //   + '<strong>Hectares: </strong>' + featureObj.properties.TENURE_AREA_IN_HECTARES
+                  //   + '<br />'
+                  //   + '<br />'
+                  //   + '<strong>Legal Description: </strong>' + featureObj.properties.TENURE_LEGAL_DESCRIPTION;
+                  // const popup = L.popup(options).setContent(content);
+                  // layer.bindPopup(popup);
+                  self.fg.addLayer(layer);
+                  layer.addTo(self.map);
+                });
+
+                const bounds = self.fg.getBounds();
+                if (!_.isEmpty(bounds)) {
+                  self.map.fitBounds(bounds);
+                }
+              },
+              error => {
+                console.log('error =', error);
+              });
           } else {
             // application not found --> navigate back to application list
             alert('Uh-oh, couldn\'t load application');
--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -3,12 +3,14 @@ import { Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
+import { Feature } from 'app/models/feature';
 
 import { ApiService } from './api';
 import { Search } from 'app/models/search';
 
 @Injectable()
 export class SearchService {
+  private features: Feature[] = null;
 
   constructor(private api: ApiService) { }
 
@@ -24,6 +26,13 @@ export class SearchService {
       .map((res: Response) => {
         const results = res.text() ? new Search(res.json()) : null;
         return results.features;
+      })
+      .map((features: Feature[]) => {
+        if (!features) { return null; }
+
+        console.log('new features =', features);
+        this.features = features;
+        return this.features;
       });
   }
 }
