11d70f2035b016e59f4bcf985fe61fb044beeef2
Mark Lisé <mark@digitalspace.ca>
2018-03-16 16:32:23 -0700

    NOBUG: Add demo

--- src/app/map/config/map-config.service.ts
+++ src/app/map/config/map-config.service.ts
@@ -62,7 +62,7 @@ export class MapConfigService {
     return `${protocol}//${host}/a/${code}`;
   }
 
-  private webmapForEnv(env: 'local' | 'dev' | 'test' | 'prod'): string {
+  private webmapForEnv(env: 'local' | 'dev' | 'test' | 'demo' | 'prod'): string {
     switch (env) {
       // local
       case 'local':
@@ -74,6 +74,10 @@ export class MapConfigService {
 
       // test
       case 'test':
+        return webmaps.test;
+
+      // demo
+      case 'demo':
         return webmaps.test;
 
       // prod
