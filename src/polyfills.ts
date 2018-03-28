b3065451ed3fd403c559ec84fee4d60265e4cb8e
Mark Lisé <mark@digitalspace.ca>
2018-03-28 14:53:55 -0700

    NOBUG: Attempting to rework polyfils and routing.

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -32,17 +32,20 @@ import 'core-js/es6/array';
 import 'core-js/es6/regexp';
 import 'core-js/es6/map';
 import 'core-js/es6/set';
+import 'core-js/es6/reflect';
+import 'core-js/client/shim';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
+import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /** IE10 and IE11 requires the following to support `@angular/animation`. */
 import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 
 /** Evergreen browsers require these. **/
-import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
+import 'core-js/es7/array';
+
 
 
 /** ALL Firefox browsers require the following to support `@angular/animation`. **/
@@ -65,4 +68,6 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  * Date, currency, decimal and percent pipes.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
  */
-// import 'intl';  // Run `npm install --save intl`.
+import 'intl';  // Run `npm install --save intl`.
+import 'intl/locale-data/complete.js';
+import 'intl/locale-data/jsonp/en.js';
