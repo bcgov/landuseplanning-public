875544885b409cf989cb500b3bda30a8e7dcf4dc
Severin Beauvais <severin.beauvais@gmail.com>
2018-05-01 16:15:21 -0700

    PRC-336: added Reset Map button

--- src/app/applications/application-detail/application-detail.component.ts
+++ src/app/applications/application-detail/application-detail.component.ts
@@ -50,7 +50,32 @@ export class ApplicationDetailComponent implements OnInit, OnDestroy {
         (data: { application: Application }) => {
           if (data.application) {
             this.application = data.application;
+
             const self = this;
+            const resetViewControl = L.Control.extend({
+              options: {
+                position: 'topleft'
+              },
+              onAdd: function (map) {
+                const container = L.DomUtil.create('i', 'material-icons leaflet-bar leaflet-control leaflet-control-custom');
+                container.style.backgroundColor = 'white';
+                container.title = 'Reset Map';
+                container.innerText = 'refresh';
+                container.style.width = '34px';
+                container.style.height = '34px';
+                container.style.lineHeight = '30px';
+                container.style.textAlign = 'center';
+                container.style.cursor = 'pointer';
+                container.onclick = function () {
+                  const bounds = self.fg.getBounds();
+                  if (!_.isEmpty(bounds)) {
+                    self.map.fitBounds(bounds, self.maxZoom);
+                  }
+                };
+                return container;
+              },
+            });
+
             // NB: always reload results to reduce chance of race condition
             //     with drawing map and features
             this.searchService.getByDTID(this.application.tantalisID, true)
@@ -75,6 +100,7 @@ export class ApplicationDetailComponent implements OnInit, OnDestroy {
                     attribution: 'Tiles &copy; Esri &mdash; National Geographic, Esri, DeLorme, NAVTEQ, UNEP-WCMC, USGS, NASA, ESA, METI, NRCAN, GEBCO, NOAA, iPC',
                     maxZoom: 16
                   });
+
                   self.map = L.map('map', {
                     layers: [World_Imagery]
                   });
@@ -88,6 +114,7 @@ export class ApplicationDetailComponent implements OnInit, OnDestroy {
                     'World Imagery': World_Imagery
                   };
                   self.control = L.control.layers(self.baseMaps, null, { collapsed: true }).addTo(self.map);
+                  self.map.addControl(new resetViewControl());
 
                   if (self.fg) {
                     _.each(self.layers, function (layer) {
