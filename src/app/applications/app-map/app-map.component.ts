72e324c9f53bce82a9ad604d808f153d0898fb65
Nick Phura <Nick.Phura@quartech.com>
2019-01-17 11:10:15 -0800

    PRC-1014: remove open surfer maps

--- src/app/applications/app-map/app-map.component.ts
+++ src/app/applications/app-map/app-map.component.ts
@@ -153,11 +153,6 @@ export class AppMapComponent implements AfterViewInit, OnChanges, OnDestroy {
       maxZoom: 16.4,
       noWrap: true
     });
-    const OpenMapSurfer_Roads = L.tileLayer('https://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}', {
-      attribution: 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-      maxZoom: 19.5,
-      noWrap: true
-    });
     const World_Topo_Map = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
       attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community',
       maxZoom: 17.5,
@@ -230,7 +225,6 @@ export class AppMapComponent implements AfterViewInit, OnChanges, OnDestroy {
     const baseLayers = {
       'Ocean Base': Esri_OceanBasemap,
       'Nat Geo World Map': Esri_NatGeoWorldMap,
-      'Open Surfer Roads': OpenMapSurfer_Roads,
       'World Topographic': World_Topo_Map,
       'World Imagery': World_Imagery
     };
