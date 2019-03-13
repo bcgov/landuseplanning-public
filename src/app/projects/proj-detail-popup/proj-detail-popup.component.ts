e44be8d339aec9cb1ecebeb73f6a77e973ee98cc
Daniel Truong <daniel@dtsoftware.ca>
2019-03-13 10:23:35 -0700

    Added comment period to map popup. Added pagination controls to top of comments

--- src/app/projects/proj-detail-popup/proj-detail-popup.component.ts
+++ src/app/projects/proj-detail-popup/proj-detail-popup.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Project } from 'app/models/project';
 import { ProjectService } from 'app/services/project.service';
 import { CommentPeriodService } from 'app/services/commentperiod.service';
+import { CommentPeriod } from 'app/models/commentperiod';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-proj-detail-popup',
@@ -11,9 +13,21 @@ import { CommentPeriodService } from 'app/services/commentperiod.service';
 
 export class ProjDetailPopupComponent {
   public proj: Project = null;
+  public commentPeriod: CommentPeriod = null;
+  public commentPeriodStatus: String;
+
+  private ngUnsubscribe: Subject<boolean> = new Subject<boolean>();
 
   constructor(
     public projectService: ProjectService, // used in template
     public commentPeriodService: CommentPeriodService // used in template
   ) { }
+
+  ngOnInit() {
+    this.commentPeriodService.getAllByProjectId(this.proj._id)
+      .takeUntil(this.ngUnsubscribe)
+      .subscribe(data => {
+          this.commentPeriodStatus = data[0].commentPeriodStatus;
+      });
+  }
 }
