fe1c233b284b9b32b5298469e915f2b9a1c64daa
Daniel Truong <daniel@dtsoftware.ca>
2019-06-07 14:48:18 -0700

    Incorperating Kirsten's code for read more/less for comments.

--- src/app/project/comments/comments-table-rows/comments-table-rows.component.ts
+++ src/app/project/comments/comments-table-rows/comments-table-rows.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, Input, ChangeDetectorRef, AfterViewInit, Renderer2 } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectorRef } from '@angular/core';
 import { ApiService } from 'app/services/api';
-
 import { Document } from 'app/models/document';
 import { TableObject } from 'app/shared/components/table-template/table-object';
 
@@ -9,18 +8,17 @@ import { TableObject } from 'app/shared/components/table-template/table-object';
   templateUrl: './comments-table-rows.component.html',
   styleUrls: ['./comments-table-rows.component.scss']
 })
-export class CommentsTableRowsComponent implements OnInit, AfterViewInit {
+
+export class CommentsTableRowsComponent implements OnInit {
   @Input() data: TableObject;
 
   public comments: any;
   public paginationData: any;
   public loading = true;
-  public showMore = false;
 
   constructor(
     private api: ApiService,
     private _changeDetectionRef: ChangeDetectorRef,
-    private renderer: Renderer2,
   ) { }
 
   ngOnInit() {
@@ -28,11 +26,10 @@ export class CommentsTableRowsComponent implements OnInit, AfterViewInit {
     this.comments = this.data.data;
     this.paginationData = this.data.paginationData;
 
-    // Scrolling while the page renders seems to cause problems with the comment section.
-    this.renderer.addClass(document.body, 'no-scroll');
-
     // Populate the documents for this item.
     this.comments.map(async comment => {
+      comment.expanded = false;
+      comment.buttonName = 'Read More';
       if (comment.documents && comment.documents.length > 0) {
         // populate the docs.
         let documents = [];
@@ -52,25 +49,15 @@ export class CommentsTableRowsComponent implements OnInit, AfterViewInit {
     });
   }
 
-  ngAfterViewInit() {
-    this.checkOverflow();
-    this.renderer.removeClass(document.body, 'no-scroll');
-    this._changeDetectionRef.detectChanges();
-  }
+  toggle(comment) {
+    comment.expanded = !comment.expanded;
 
-  public toggleShowMore(index) {
-    let commentElement = document.getElementById(index);
-    commentElement.className === 'show-less' ? commentElement.className = '' : commentElement.className = 'show-less';
-  }
-
-  public checkOverflow() {
-    Array.from(document.getElementsByClassName('comment')).forEach(element => {
-      if (element.clientHeight < element.scrollHeight) {
-        element.className = 'show-less';
-      } else {
-        document.getElementById('read-button-' + element.id).remove();
-      }
-    });
+    // CHANGE THE NAME OF THE BUTTON.
+    if (comment.expanded) {
+      comment.buttonName = 'Read Less';
+    } else {
+      comment.buttonName = 'Read More';
+    }
   }
 
   public openAttachment(file) {
